1749f81e1760aba7f305115bd83f9c1a503260be
juleenek <leeenek0@gmail.com>
2023-04-27 21:14:30 +0200

    First routing and login panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { User } from './models/user.model';
+import { UserRole } from './models/enums/role.enum';
+
+const IS_LOGGED_DEFAULT = false;
+const ADMIN: User = {
+  id: 1,
+  login: 'admin',
+  first_name: 'admin',
+  last_name: 'admin',
+  role: UserRole.ADMIN,
+  password: 'p@$$word',
+};
+const users: User[] = [];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'manage-me';
+  constructor() {
+    users.push(ADMIN);
+    localStorage.setItem('users', JSON.stringify(users));
+    localStorage.setItem('isLogged', JSON.stringify(IS_LOGGED_DEFAULT));
+  }
 }
